ab1464cac4ad7d7a06e38e713841e3eef1e7e389
Your Name <your@email.com>
2025-04-16 14:24:09 +0530

    first commit

---
+++ src/main/java/com/maventproject10/maventest10/App.java
@@ -0,0 +1,13 @@
+package com.maventproject10.maventest10;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
